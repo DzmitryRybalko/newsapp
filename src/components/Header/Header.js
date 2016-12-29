87843f15c4cccf5115e4a59ddc167b93436f9f68
dzmitry_rybalko <rbibuk@gmail.com>
2016-12-30 00:35:00 +0300

    Add reload counter

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,31 @@ import React from 'react';
 import './Header.scss';
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    let reloadTimes = window.localStorage.getItem('reloadTimes') || 0;
+    ++reloadTimes;
+    window.localStorage.setItem('reloadTimes', reloadTimes);
+
+    this.state = {
+      reloadTimes,
+    };
+  }
+
+  componentDidMount() {
+    console.log('mount');
+    setTimeout(() => {
+
+    }, 1000 * 60 * 60 * 24);
+  }
+
   render() {
     const time = new Date(Date.now()).toDateString();
     return (
       <div className="header">
         <div className="container">
           <div className="title">{`News App - ${time}`}</div>
+          <div className="reloads">You load this page {this.state.reloadTimes} times.</div>
         </div>
       </div>
     );
